21e57692ed7b1b8cc32410462e41c9779f0b9743
mnhthompson <mnhthompson@gmail.com>
2025-11-18 14:58:55 -0700

    123

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,10 @@ if (startBtn) {
     window.location.href = './team/index.html';
   });
 }
+
+const saveBtn = document.getElementById('load-saved-team');
+if (saveBtn) {
+  saveBtn.addEventListener('click', () => {
+    window.location.href = './save/index.html';
+  });
+}
